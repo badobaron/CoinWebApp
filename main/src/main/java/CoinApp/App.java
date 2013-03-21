d3bbc84d4da29e9e49a572593653694b385c6df0
taitgr <gabriel.tait@issinc.com>
2013-03-20 20:41:14 -0600

    initial archetype build

---
+++ main/src/main/java/CoinApp/App.java
@@ -0,0 +1,13 @@
+package CoinApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
